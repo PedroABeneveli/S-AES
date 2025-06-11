247362d3b2f5cd46b0392cc607039fc706b8c68f
Pedro Beneveli <pedro.beneveli@gmail.com>
2025-06-11 17:55:25 -0300

    SubNibbles

--- S-AES/headers/operations.h
+++ S-AES/headers/operations.h
@@ -7,4 +7,7 @@
 // Funcao addRoundKey, que realiza adicao do dado com a chave modulo 2, ou seja, realiza um XOR entre a entrada e a chave dada
 NibbleMatrix addRoundKey(NibbleMatrix data, NibbleMatrix key, bool print=false);
 
+// Uso de S-Box pra cada Nibble da Matriz
+NibbleMatrix subNibbles(NibbleMatrix data, bool print=false);
+
 #endif
--- S-AES/src/operations.cpp
+++ S-AES/src/operations.cpp
@@ -13,3 +13,35 @@ NibbleMatrix addRoundKey(NibbleMatrix data, NibbleMatrix key, bool print) {
 
     return result;
 }
+
+// S-Box fixa utilizada na funcao subNibbles
+bitset<4> sbox[4][4] = {
+    {bitset<4>(9), bitset<4>(4), bitset<4>(10), bitset<4>(11)},
+    {bitset<4>(13), bitset<4>(1), bitset<4>(8), bitset<4>(5)},
+    {bitset<4>(6), bitset<4>(2), bitset<4>(0), bitset<4>(3)},
+    {bitset<4>(12), bitset<4>(14), bitset<4>(15), bitset<4>(7)}
+};
+
+NibbleMatrix subNibbles(NibbleMatrix data, bool print) {
+    NibbleMatrix result;
+    int num;
+
+    for (int i = 0 ; i < 2 ; i++) {
+        for (int j = 0 ; j < 2 ; j++) {
+            num = (int)(data.nibbles[i][j].to_ulong());
+
+            // uso os 2 bits mais significativos para o indice da linha, e isolo os 2 menos para o indice da coluna
+            result.nibbles[i][j] = sbox[num>>2][num&3];
+        }
+    }
+
+    if (print) {
+        cout << "subNibbles:\n";
+        cout << "    |   Data    |  Subbed   |\n";
+        cout << "    | " << data.nibbles[0][0] << " " << data.nibbles[0][1] << " | " << result.nibbles[0][0] << " " << result.nibbles[0][1] << " |\n";
+        cout << "    | " << data.nibbles[1][0] << " " << data.nibbles[1][1] << " | " << result.nibbles[1][0] << " " << result.nibbles[1][1] << " |\n";
+        cout << "\n";
+    }
+
+    return result;
+}
